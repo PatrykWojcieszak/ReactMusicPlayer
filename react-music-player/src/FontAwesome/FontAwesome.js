a9a5bb0e2080aca2d5f26b16763fa3ca8a08be61
Patryk Wojcieszak <patryk.p.wojcieszak@gmail.com>
2020-11-11 13:54:49 +0100

    feat: Added songs, sass, fontawesome

---
+++ react-music-player/src/FontAwesome/FontAwesome.js
@@ -0,0 +1,12 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+  faPause,
+  faVolumeDown,
+  faMusic,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPlay, faAngleLeft, faAngleRight, faPause, faVolumeDown, faMusic);
